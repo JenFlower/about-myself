454a849f75cfe173933687304a63cef63b6056c1
Olga Terekhova <jenflower97@gmail.com>
2023-08-25 22:34:01 +0300

    feature: добавила слайдер с информацией о себе

---
+++ src/app/components/Slider.jsx
@@ -0,0 +1,93 @@
+"use client";
+import React, { useState, useEffect, useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { DotButton, PrevButton, NextButton } from "./SliderButtons";
+import Image from "next/image";
+import { chooseDataSlide, chooseDataSlides } from "@/app/utils/constants";
+
+export const Slider = ({ slides, options }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi],
+  );
+
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi],
+  );
+
+  const scrollTo = useCallback(
+    (index) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi],
+  );
+
+  const onInit = useCallback((emblaApi) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onInit);
+    emblaApi.on("reInit", onSelect);
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
+  return (
+    <section className="slider">
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {slides.map((index) => {
+              const currentSlide = chooseDataSlide(index);
+              return (
+                <div className="embla__slide" key={index}>
+                  <Image
+                    className="embla__slide__img"
+                    src={currentSlide.url}
+                    alt="Фото со мной"
+                  />
+                  <div className="embla__slide__content">
+                    <h3 className="embla__slide__title">{currentSlide.name}</h3>
+                    <p className="embla__slide__desc">{currentSlide.desc}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="embla__buttons">
+          <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+          <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => scrollTo(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot_selected" : "",
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import styles from "./page.module.css";
-import { Header } from "./components/Header";
+import { Header } from "@/app/components/Header";
+import { Main } from "@/app/components/Main";
 
 export default function Home() {
   return (
@@ -7,71 +7,8 @@ export default function Home() {
       <div className="background" />
       <div className="background background_primary" />
       <div className="background background_secondary" />
-      <main className="main">
-        <Header />
-        <div className="main__title">
-          Меня зовут Оля и я - фронтенд-разработчик.{" "}
-          <span className="main__title_accent-main">Кодю</span> сайты и
-          приложения. Мне нравится расти, обучаться и{" "}
-          <span className="main__title_accent-secondary">
-            помогать в этом коллегам
-          </span>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Header />
+      <Main />
     </>
   );
 }
